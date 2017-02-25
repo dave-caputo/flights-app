7575f912373a28ad3d0f9b4e1592251774390a62
davecaputo <davecaputo@hotmail.com>
2017-02-25 14:09:31 +0000

    Enroute model migrated and tested.

--- flights/data/models.py
+++ flights/data/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Enroute(models.Model):
-    ident = models.CharField(max_length=255)            # "NAX1318"
-    aircrafttype = models.CharField(max_length=255)     # "B738"
-    actualdeparturetime = models.IntegerField()         # 0
-    estimatedarrivaltime = models.IntegerField()        # 1486930380
-    filed_departuretime = models.IntegerField()         # 1486923000
-    origin = models.CharField(max_length=255)           # "ENBR"
-    destination = models.CharField(max_length=255)      # "EGKK"
-    originName = models.CharField(max_length=255)       # "Bergen, Flesland"
-    originCity = models.CharField(max_length=255)       # "Bergen, Hordaland"
-    destinationName = models.CharField(max_length=255)  # "London Gatwick"
-    destinationCity = models.CharField(max_length=255)  # "London, England"
+    ident = models.CharField(max_length=255, default='')            # "NAX1318"
+    aircrafttype = models.CharField(max_length=255, default='')     # "B738"
+    actualdeparturetime = models.IntegerField(default=0)            # 0
+    estimatedarrivaltime = models.IntegerField(default=0)           # 1486930380
+    filed_departuretime = models.IntegerField(default=0)            # 1486923000
+    origin = models.CharField(max_length=255, default='')           # "ENBR"
+    destination = models.CharField(max_length=255, default='')      # "EGKK"
+    originName = models.CharField(max_length=255, default='')       # "Bergen, Flesland"
+    originCity = models.CharField(max_length=255, default='')       # "Bergen, Hordaland"
+    destinationName = models.CharField(max_length=255, default='')  # "London Gatwick"
+    destinationCity = models.CharField(max_length=255, default='')  # "London, England"
--- flights/data/tests.py
+++ flights/data/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from data.models import Enroute
+
+
+class ListAndEnrouteModelsTest(TestCase):
+
+    def test_default_fields(self):
+        flight = Enroute()
+        self.assertEqual(flight.ident, '')
+        self.assertEqual(flight.aircrafttype, '')
+        self.assertEqual(flight.actualdeparturetime, 0)
+        self.assertEqual(flight.estimatedarrivaltime, 0)
+        self.assertEqual(flight.filed_departuretime, 0)
+        self.assertEqual(flight.origin, '')
+        self.assertEqual(flight.destination, '')
+        self.assertEqual(flight.originName, '')
+        self.assertEqual(flight.originCity, '')
+        self.assertEqual(flight.destinationName, '')
+        self.assertEqual(flight.destinationCity, '')
