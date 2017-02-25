d53bddbd22024b977169aef6e78aff71fe260608
davecaputo <davecaputo@hotmail.com>
2017-02-25 11:30:39 +0000

    Enroute model created in data app matching the types of json input fields.

--- flights/data/models.py
+++ flights/data/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Enroute(models.Model):
+    ident = models.CharField(max_length=255)            # "NAX1318"
+    aircrafttype = models.CharField(max_length=255)     # "B738"
+    actualdeparturetime = models.IntegerField()         # 0
+    estimatedarrivaltime = models.IntegerField()        # 1486930380
+    filed_departuretime = models.IntegerField()         # 1486923000
+    origin = models.CharField(max_length=255)           # "ENBR"
+    destination = models.CharField(max_length=255)      # "EGKK"
+    originName = models.CharField(max_length=255)       # "Bergen, Flesland"
+    originCity = models.CharField(max_length=255)       # "Bergen, Hordaland"
+    destinationName = models.CharField(max_length=255)  # "London Gatwick"
+    destinationCity = models.CharField(max_length=255)  # "London, England"
