b46f43c38c0203838a042bf79c5cc368729420be
davecaputo <davecaputo@hotmail.com>
2017-02-26 19:21:54 +0000

    Home page now gets information from cache and loads to homepage.

--- flights/home/models.py
+++ flights/home/models.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
+from django.core.cache import cache
 
 from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
-    pass
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['enroute'] = cache.get('Enroute', 'Not found')
+        return context
