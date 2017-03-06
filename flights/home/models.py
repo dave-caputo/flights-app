742f4700fffed7cae7122203039163fa7575428e
davecaputo <davecaputo@hotmail.com>
2017-03-06 08:40:14 +0000

    Home page model now displays flights from multiple airports.

--- flights/home/models.py
+++ flights/home/models.py
@@ -10,5 +10,12 @@ class HomePage(Page):
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['enroute'] = cache.get('enroute_EGKK', 'Not found')
+
+        flights = []
+        airports = ('EGLL', 'EGKK')
+        for a in airports:
+            c = cache.get('enroute_{}'.format(a), 'Not found')
+            flights += c
+
+        context['enroute'] = flights
         return context
