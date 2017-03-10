cc3e9ba3d1213112bd99b8ae2282c17533c0b5cd
davecaputo <davecaputo@hotmail.com>
2017-03-10 12:34:08 +0000

    Basic Arrived url view and template set up with no data being retrieved yet.

--- flights/data/urls.py
+++ flights/data/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from data.views import EnrouteFlightsView
+from data.views import EnrouteFlightsView, ArrivedFlightsView
 
 urlpatterns = [
     url(r'^enroute/', EnrouteFlightsView.as_view(), name='enroute'),
+    url(r'^arrived/', ArrivedFlightsView.as_view(), name='arrived'),
 ]
--- flights/data/views.py
+++ flights/data/views.py
@@ -11,3 +11,11 @@ class EnrouteFlightsView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['enroute'] = get_enroute_flights()
         return context
+
+class ArrivedFlightsView(TemplateView):
+    template_name = 'data/arrived.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['arrived'] = get_enroute_flights()
+        return context
