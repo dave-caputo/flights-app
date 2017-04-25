acd75b3a7bf1f7dd03c5da5bd6f308fce59a7620
davecaputo <davecaputo@hotmail.com>
2017-04-25 20:08:47 +0100

    auto_now fields added to City model for created and modified.

--- flights/cities/models.py
+++ flights/cities/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class City(models.Model):
     city = models.fields.CharField(max_length=255, unique=True)
     iata = models.fields.CharField(max_length=255, unique=True)
+    created = models.fields.DateTimeField(auto_now_add=True)
+    modified = models.fields.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'cities'
