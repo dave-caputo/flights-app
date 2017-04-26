85215a65643d6f1995d7a3542ae40b8f80174f79
davecaputo <davecaputo@hotmail.com>
2017-04-26 08:44:18 +0100

    Unique only constraint removed for City model field 'city'.

--- flights/cities/models.py
+++ flights/cities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    city = models.fields.CharField(max_length=255, unique=True)
+    city = models.fields.CharField(max_length=255)
     iata = models.fields.CharField(max_length=255, unique=True)
     created = models.fields.DateTimeField(auto_now_add=True)
     modified = models.fields.DateTimeField(auto_now=True)
