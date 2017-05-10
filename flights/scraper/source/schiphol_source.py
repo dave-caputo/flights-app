4dc217ca2e17bb147d70b926d111ef12ddd758ab
davecaputo <davecaputo@hotmail.com>
2017-05-10 11:25:37 -0500

    Docstrings written for all methods in SchipholFlightManager and remove old commented-out code.

--- flights/scraper/source/schiphol_source.py
+++ flights/scraper/source/schiphol_source.py
@@ -18,6 +18,10 @@ from scraper.source.utils import format_to_data_table, merge_codeshare_flights
 
 
 class SchipholFlightManager:
+    '''
+    Provides methods to request flights from API and process data to
+    return template-friendly flights lists.
+    '''
 
     def __init__(self, operation, carousel):
         self.operation = operation
@@ -42,12 +46,20 @@ class SchipholFlightManager:
 
 
     def get_local_datetime(self):
+        '''
+        Returns the local datetime based on the UTC time and the local
+        time zone provided on initialization.
+        '''
         utc_datetime = timezone.now()
         local_datetime = utc_datetime.astimezone(self.local_timezone)
         return local_datetime
 
 
     def set_time_limits(self, max_minutes=None, min_minutes=None):
+        '''
+        Sets the maximum and minimum time limits outside which flights
+        will not be retrieved.
+        '''
         if max_minutes and min_minutes:
             self.max_datetime = self.local_datetime + timedelta(minutes=max_minutes)
             self.min_datetime = self.local_datetime - timedelta(minutes=min_minutes)
@@ -58,6 +70,10 @@ class SchipholFlightManager:
 
 
     def make_request(self, page=None, str_time=0):
+        '''
+        Makes a request to the Schiphol Airport API based on the
+        object's url, headers and params. Returns a response object.
+        '''
         attempt = 0
         valid_response = False
 
@@ -83,7 +99,10 @@ class SchipholFlightManager:
 
 
     def get_scheduled_datetime(self, flight):
-
+        '''
+        Translate the scheduled time provided in response as a string,
+        to a datetime object.
+        '''
         str_scheduled_time = flight['scheduleTime']
         t = datetime.strptime(str_scheduled_time, '%H:%M:%S')
         d = datetime.combine(self.local_datetime.date(), t.time())
@@ -93,6 +112,12 @@ class SchipholFlightManager:
 
 
     def get_city_name(self, flight):
+        '''
+        Matches the city code in the response to the city name, seeking
+        first in the database. If the city name is not found, it will
+        get it via make an API request and save the city name to the
+        database.
+        '''
         print('Retrieving city name...')
 
         mgr = SchipholCityManager()
@@ -112,6 +137,11 @@ class SchipholFlightManager:
 
 
     def update_flight_data(self, flight_list):
+        '''
+        For the flight list in the API response, transforms each field
+        for processing and displaying correctly the data in the
+        template.
+        '''
         print('Updating data...')
 
         updated_flight_list = []
@@ -200,7 +230,9 @@ class SchipholFlightManager:
 
 
     def get_page_count(self):
-
+        '''
+        Makes an API request to obtain from the headers the page count.
+        '''
         response = self.make_request()
 
         # Obtain from headers and cache the number of pages
@@ -215,7 +247,10 @@ class SchipholFlightManager:
     @format_to_data_table
     @merge_codeshare_flights
     def get_flights(self):
-
+        '''
+        Returns a template-friendly sorted flight list to display in
+        carousel or full list views.
+        '''
         if self.carousel:
             self.set_time_limits(max_minutes=120, min_minutes=60)
 
@@ -244,304 +279,11 @@ class SchipholFlightManager:
 
 
 def get_schiphol_flights(operation, carousel=False):
+    '''
+    Instantiates a SchipolFlightManager object specifying the operation
+    and whether the data should be customised for displaying in a
+    carousel.
+    '''
     m = SchipholFlightManager(operation, carousel)
     flights = m.get_flights()
     return flights
-
-
-
-
-'''
-def update_flight_data(flights, local_datetime):
-
-    # destinations = cache.get('destinations')
-    pages_cached = cache.get('dest_pages_cached')
-    cache_block_size = 20
-    updated_flight_list = []
-
-    city_mgr = SchipholCityManager()
-
-    for f in flights:
-
-        str_scheduled_time = f['scheduleTime']
-        t = datetime.strptime(str_scheduled_time, '%H:%M:%S')
-        d = datetime.combine(local_datetime['now'].date(), t.time())
-        scheduled_datetime = local_datetime['tz'].localize(d)
-
-        if local_datetime['time_limits']:
-
-            # Stop the loop if max_time_limit is exceeded.
-            if scheduled_datetime > local_datetime['max_time']:
-                updated_flight_list.append("Max_time_exceeded")
-                break
-
-        # Consider only Passenger Line services
-        if f['serviceType'] != "J":
-            continue
-
-        # Key aligned with template and 'seconds' info sliced off.
-        f['scheduledTimestamp'] = f.pop('scheduleTime')[:-3]
-
-        # Translate city code to city name.
-        route = f.pop('route')
-        city_code = route['destinations'][0]
-        try:
-            f['city'] = City.objects.get(iata=city_code).city
-        except:
-            f['city'] = city_mgr.get_and_save_city(iata=city_code)
-        """
-        try:
-            for block in range(20, pages_cached + 1, cache_block_size):
-                first = cache.get('dest_{}_start'.format(block))
-                print(first)
-                last = cache.get('dest_{}_end'.format(block))
-
-                if city_code >= first and city_code <= last:
-                    block_data = cache.get('dest_{}'.format(block))
-
-                    f['city'] = (
-                        dest['city'] for dest in block_data if dest['iata'] == city_code
-                    ).__next__()
-        except StopIteration:
-
-            f['city'] = 'Not Available'
-        """
-        f['flightNumber'] = f.pop('flightName')
-        f['terminalId'] = f.pop('terminal', 'Not Available')
-
-        # Prepare data for translating status
-        if f['actualOffBlockTime']:
-            aobt = f['actualOffBlockTime'][11:-13]
-        else:
-            aobt = f['scheduledTimestamp']
-
-        if f['publicEstimatedOffBlockTime']:
-            peobt = f['publicEstimatedOffBlockTime'][11:-13]
-        else:
-            peobt = f['scheduledTimestamp']
-
-        if f['actualLandingTime']:
-            alt = f['actualLandingTime'][11:-13]
-        else:
-            alt = f['scheduledTimestamp']
-
-        if f['estimatedLandingTime']:
-            elt = f['estimatedLandingTime'][11:-13]
-        else:
-            elt = f['scheduledTimestamp']
-
-        # Translate status.
-        status = f.pop('publicFlightState')
-        status = status['flightStates'][0]
-        status_list = {
-            # Arrivals
-            'AIR': 'Expected {}'.format(elt),
-            'ARR': 'Arrived {}'.format(alt),
-            'EXP': 'Expected {}'.format(elt),
-            'FIB': 'Expected {}'.format(elt),
-            'FIR': 'Expected {}'.format(elt),
-            'LND': 'Landed {}'.format(alt),
-
-            # Departures
-            'BRD': 'Boarding - Departing {}'.format(peobt),
-            'DEL': 'Delayed {}'.format(peobt),
-            'DEP': 'Departed {}'.format(aobt),
-            'CNX': 'Cancelled',
-            'GCH': 'Gate changed - Departing {}'.format(peobt),
-            'GTO': 'Gate {} open - Departing {}'.format(f['gate'], peobt),
-            'GCL': 'Gate {} closing - Departing {}'.format(f['gate'], peobt),
-            'GTD': 'Gate closed - Departing {}'.format(peobt),
-            'SCH': 'Scheduled',
-            'TOM': 'Delayed tomorrow {}'.format(peobt),
-            'WIL': 'Wait in lounge. Departing {}'.format(peobt),
-        }
-
-        f['flightOutputStatus'] = status_list.get(status, status)
-
-        updated_flight_list.append(f)
-
-    return updated_flight_list
-
-
-def get_flight_page_count(operation, local_datetime):
-    response = get_response(operation, local_datetime)
-    link = response.headers['Link']
-    page_count = re.findall(r'(?<=page\=)\d+', link)[0]
-    return int(page_count)
-
-@format_to_data_table
-@merge_codeshare_flights
-def get_schiphol_flights(operation, carousel=False):
-
-    if carousel:
-        local_datetime = get_local_datetime(max_interval=120, min_interval=60)
-    else:
-        local_datetime = get_local_datetime()
-
-    flight_list = []
-    page_count = get_flight_page_count(operation, local_datetime)
-    page = 0
-
-    for page in range(0, page_count + 1):
-        response = get_response(operation, local_datetime, page)
-
-        # Check response type
-        if response.status_code == 500:
-            print('Http 500: API responded with an internal server error.')
-            break
-
-        elif response.status_code == 200:
-            response_data = response.json()
-
-            # For each flight align keys and values with template.
-            flights = update_flight_data(response_data['flights'],
-                                         local_datetime)
-
-            if flights and flights[-1] == "Max_time_exceeded":
-                break
-
-            flight_list += flights
-
-    # Cache if the flight list is not empty:
-    if flight_list:
-        cache.set('schiphol_{}'.format(operation), flight_list)
-
-    return sorted(flight_list, key=itemgetter('scheduledTimestamp', 'city'))
-
-
-def get_response(operation, local_datetime, page=0):
-
-    # More info at https://developer.schiphol.nl/apis/flight-api/flights
-    url = 'https://api.schiphol.nl/public-flights/flights'
-    p = {
-        'app_id': '7e07d59a',
-        'app_key': settings.SCHIPHOL_KEY,
-        'flightdirection': operation[0].upper(),
-        'includedelays': 'true',
-        'page': page,
-        'scheduletime': local_datetime.get('str_min_time', None),
-        'sort': '+scheduleTime'  # No more filters available: API bug!
-    }
-    h = {'resourceversion': 'v3'}
-    try:
-        response = requests.request("GET", url, headers=h, params=p)
-        return response
-    except requests.exceptions.ConnectionError as error:
-        print('Unable to get data from page {}'.format(page))
-'''
-'''
-def get_local_datetime(max_interval=None, min_interval=None):
-
-    # To get the list of all timezones loop over pytz.all_timezones
-    # More info at https://www.youtube.com/watch?v=eirjjyP2qcQ
-    utc_now = timezone.now()
-    amsterdam_tz = pytz.timezone('Europe/Amsterdam')
-    now = utc_now.astimezone(amsterdam_tz)
-
-    local_datetime = {
-        'now': now,
-        'time_limits': False,
-        'tz': amsterdam_tz
-    }
-
-    if max_interval and min_interval:
-        str_time = lambda x: datetime.strftime(x, '%H:%M')
-        local_datetime['time_limits'] = True
-
-        max_time = now + timedelta(minutes=max_interval)
-        local_datetime['max_time'] = max_time
-        local_datetime['str_max_time'] = str_time(max_time)
-
-        min_time = now - timedelta(minutes=min_interval)
-        local_datetime['min_time'] = min_time
-        local_datetime['str_min_time'] = str_time(min_time)
-
-    return local_datetime
-'''
-
-'''
-def get_destinations():
-
-    url = 'https://api.schiphol.nl/public-flights/destinations'
-    p = {
-        'app_id': '7e07d59a',
-        'app_key': settings.SCHIPHOL_KEY,
-        'sort': '+iata'
-    }
-    h = {
-        'resourceversion': 'v1'
-    }
-
-    response = requests.request("GET", url, headers=h, params=p)
-    if response.status_code == 200:
-
-        # Obtain from headers and cache the number of pages
-        link = response.headers['Link']
-        page_count = int(re.findall(r'(?<=page\=)\d+', link)[0])
-        cache.set('dest_page_count', page_count)
-        print('Page count obtained and cached: {}'.format(cache.get('dest_page_count')))
-
-        # Set how data will be cached
-        page_block = []
-        page_block_size = 20
-
-        for page in range(1, page_count + 1):
-
-            # Include page as a request parameters
-            p['page'] = page
-
-            # Retry until response status code 200
-            attempt = 0
-            valid_response = False
-            while not valid_response:
-                response = requests.request("GET", url, headers=h, params=p)
-                print(response)
-                if response.status_code == 200:
-                    valid_response = True
-                attempt += 1
-                if attempt == 30:
-                    continue
-
-            d = response.json()
-
-            updated_dest = []
-            for item in d['destinations']:
-                u = {'iata': item['iata'], 'city': item['city']}
-                updated_dest.append(u)
-
-            page_block += updated_dest
-
-            if page % page_block_size == 0 or page == page_count:
-
-                # Set up cache key names
-                key = 'dest_{}'.format(page)
-                timestamp_key = key + '_timestamp'
-                start_key = key + '_start'
-                end_key = key + '_end'
-
-                s = page_block[0]['iata']
-                if not s:
-                    s = 'ZZZ'
-
-                e = page_block[-1]['iata']
-                if not e:
-                    e = 'ZZZ'
-
-                # Set cache values
-                cache.set(key, page_block)
-                cache.set(timestamp_key, timezone.now())
-                cache.set(start_key, )
-                cache.set(end_key, page_block[-1]['iata'])
-
-                cache.set('dest_pages_cached', page)
-
-                msg = 'Page {}: {} destinations were cached.'
-                print(msg.format(page, len(page_block)))
-                print('Block start: {}. End:{}.'.format(cache.get(start_key), cache.get(end_key)))
-
-                page_block = []
-
-        return 'Destinations successfully cached'
-    else:
-        print(response)
-'''
